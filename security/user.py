e2e02832401054f4d9dad6c023aecee90d167823
Fiz0rg <roma_03-96@mail.ru>
2022-08-17 19:23:29 +0300

    just a little update

--- security/user.py
+++ security/user.py
@@ -1,4 +1,19 @@
 from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def verify_password(planed_password, hashed_password):
+    return pwd_context.verify(planed_password, hashed_password)
+
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(): 
+    pass
